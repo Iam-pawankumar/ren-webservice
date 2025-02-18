fc7ab8dcbeedd7bed529673e3179773444e16591
Karamtoth PawanKumar <pawankumarkaramtoth@gmail.com>
2025-02-18 14:09:15 +0530

    [App setup]

---
+++ src/main/java/com/koder/ren_webservice/RenWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.koder.ren_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenWebserviceApplication.class, args);
+	}
+
+}
